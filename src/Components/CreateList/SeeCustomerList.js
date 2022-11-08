6f1e302b38d0a7812ade22cc8580d0bd059cc2e9
JRnihar <niharmonirulislam@gmail.com>
2022-11-08 11:36:02 +0600

    update Ui

--- src/Components/CreateList/SeeCustomerList.js
+++ src/Components/CreateList/SeeCustomerList.js
@@ -21,7 +21,7 @@ const SeeCustomerList = ({list}) => {
         <div className='col p-3 mt-5 '>
             <Card className='pt-2 pb-2 border-bottom-0  border-5 border border-dark  border-start-0 me-3  d-flex flex-column align-items-center justify-content-center shadow-lg  mt-5' style={{ width: '20rem', background: '#4d4d4f', borderRadius: "20px" }} >
                 <Card.Body  >
-                    <p className='text-white' style={{fontSize: ` ${list?.allValues?.sections[1].setting.customCSS.fontSize}`, textAlign:`${list?.allValues?.sections[1].setting.customCSS.textAlign}`, fontFamily: `${list.allValues?.sections[1].setting.customCSS.fontFamily}`, color: `${list.allValues?.sections[1].setting.customCSS.color}` }}>Name : {list?.allValues?.sections[1].setting?.customCSS?.text}</p>
+                    <p  style={{fontSize: `${list?.allValues?.sections[1].setting.customCSS.fontSize}`, textAlign:`${list?.allValues?.sections[1].setting.customCSS.textAlign}`, fontFamily: `${list.allValues?.sections[1].setting.customCSS.fontFamily}`, color:`${list.allValues?.sections[1].setting.customCSS.color}` }}>Name : {list?.allValues?.sections[1].setting?.customCSS?.text}</p>
                     <p className='text-white'>Email : {email}</p>
                     <div class="d-grid gap-2">
                         <button style={{ backgroundColor: `${list?.allValues?.sections[4].setting.customCSS.backgroundColor}`, textAlign: ` ${list?.allValues?.sections[4].setting.customCSS.textAlign}`, }} class="btn btn-success mt-3" type="button">{list?.allValues?.sections[4].setting.customCSS.text ? list?.allValues?.sections[4].setting.customCSS.text : 'Subcribe'}</button>
